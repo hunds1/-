38b07d28b7bbd4e897eb15fc4c8513babc4af995
YanaP <iana.pavlova@urfu.me>
2022-11-19 16:55:36 +0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Document doc = Jsoup.connect("https://www.logobook.ru/").get();
+
+            Elements imgElements = doc.select("img");
+            Elements aElements = doc.select("a");
+            Elements divElements = doc.select("div.cl_l");
+
+            divElements.forEach(divElement ->{
+                String onclick = divElement.attr("onclick");
+                System.out.println("https://www.logobook.ru/prod_show." + " | " + onclick);
+            });
+
+            aElements.forEach(aElement ->{
+                String a = aElement.attr("href");
+                System.out.println(a);
+            });
+
+            imgElements.forEach(imgElement ->{
+                String onclick = imgElement.attr("src");
+                if (onclick.contains("https://www."))
+                    return;
+                else
+                    System.out.println("https://www.logobook.ru/"+onclick);
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
